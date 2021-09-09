32aa616584b25a0526ce1f85975337b15be9f36a
Crowdbotics <team@crowdbotics.com>
2021-09-09 14:55:32 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "flex_30417Identifier",
+  url: "https://flex_30417Identifier.botics.co",
+  api: "https://flex_30417Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
